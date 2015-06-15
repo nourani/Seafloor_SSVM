68baddc61d069537ff62f11cfc74be92978ad593
Roberto Lopez-Sastre <robertoj.lopez@uah.es>
2015-06-15 12:32:42 +0200

    compile.sh file added

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Compile the project
+
+echo "Compiling Hierarchical library"
+cd Hierarchical/
+mkdir build
+cd build
+cmake ../
+make
+cp libHierarchical.a ../../libHierarchical.a
+cd ../../
+
+echo "Compiling the Structured SVM code for hierarchical classification"
+make clean
+make all
